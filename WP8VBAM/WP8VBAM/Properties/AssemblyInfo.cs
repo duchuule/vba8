8667fcbb9583f92e2747a135356c12f09adb397e
Duc Le <duchuule@users.noreply.github.com>
2015-02-02 17:56:43 +0000

    change copyright year

--- WP8VBAM/WP8VBAM/Properties/AssemblyInfo.cs
+++ WP8VBAM/WP8VBAM/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VBA8")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
